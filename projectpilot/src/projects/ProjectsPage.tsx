8b558f4169c184e21daecd22c3c66befe38518e2
Emmanuel Asante <emmaasante676@gmail.com>
2025-06-08 23:49:49 +0000

    import MOCK_PROJECTS into project page component

--- projectpilot/src/projects/ProjectsPage.tsx
+++ projectpilot/src/projects/ProjectsPage.tsx
@@ -1,7 +1,12 @@
+import { MOCK_PROJECTS } from "./MockProjects";
+
 function ProjectsPage() {
   return (
-    <div>Projects</div>
-  )
+    <>
+      <div>Projects</div>
+      <pre>{JSON.stringify(MOCK_PROJECTS, null, " ")}</pre>
+    </>
+  );
 }
 
-export default ProjectsPage
+export default ProjectsPage;
